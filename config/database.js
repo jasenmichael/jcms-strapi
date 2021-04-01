e5510eb2bb62e9ea022d82c6961948966d2cefe6
jasenmichael <jasen@ready-builder.com>
2021-03-31 21:03:35 -0500

    chore: install and config db

---
+++ config/database.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        // schema: 'public',
+        ssl: env.bool('DATABASE_SSL'),
+      },
+      options: {},
+    },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '586e3817-ee92-4e23-a582-4b04bf8d6fe7'
+};

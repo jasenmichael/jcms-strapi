cebe15c82c7006ff1768fdc74947bf5106265dda
jasenmichael <jasen@ready-builder.com>
2021-03-31 22:53:02 -0500

    chore: install and config db

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host:  env('HOST', process.env.HOST || process.env.HEROKU_URL), //env('HOST'),
-  port: env.int('PORT', process.env.PORT || 80),
+  port: env.int('PORT', process.env.PORT || 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e50c90ed2f94fe74f02292126b7cb70d'),

8ea2122ed3ff34a2d4d7d0893afdb4847e1de9b6
jasenmichael <jasen@ready-builder.com>
2021-03-31 23:03:42 -0500

    chore: install and config db

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', process.env.PORT || 8080),
   admin: {
     auth: {
+      autoOpen: false,
       secret: env('ADMIN_JWT_SECRET', 'e50c90ed2f94fe74f02292126b7cb70d'),
     },
   },

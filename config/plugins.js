9263f162e9ed93d62e2f5fe3bdcad90026112fd7
jasenmichael <jasen@ready-builder.com>
2021-04-09 17:57:42 -0500

    chore: add dropbox providerOptions

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
     upload: {
-      provider: 'dropbox',
+      provider: 'dropbox-v2',
       providerOptions: {
         accessToken: env('DROPBOX_TOKEN'),
+        defaultUploadDir: "/uploads/"
       },
     },
   });

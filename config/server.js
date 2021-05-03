8f75d2f79cb195c140f9af3f30ce751960c06d88
jasenmichael <jasen@ready-builder.com>
2021-05-03 00:44:12 -0500

    chore: watch ignore db exports

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
-  host:  env('HOST',  '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 8080),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", process.env.PORT || 8080),
+  frontendUrl: env("FRONTEND_URL", "http://localhost:3000"),
   admin: {
     auth: {
       autoOpen: false,
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
+    watchIgnoreFiles: ["**/exports/**"],
   },
 });

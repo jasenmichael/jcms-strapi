908e13bffe3fe83eff53b525da15ccd8a368695d
jasenmichael <jasen@ready-builder.com>
2021-05-03 00:28:48 -0500

    chore: implement logout query to window.parent if in iframe

--- admin/src/containers/Admin/Logout/index.js
+++ admin/src/containers/Admin/Logout/index.js
@@ -5,7 +5,8 @@
  */
 
 /* eslint-disable */
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormattedMessage } from "react-intl";
 import { withRouter } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -21,11 +22,41 @@ import Wrapper from "./components";
 
 const Logout = ({ history: { push } }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [userId, setUserID] = useState(null);
+  // let userId = null;
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const adminEmail = auth.getUserInfo().email;
+    // get admin's user id by admin email
+    const adminUserId = await fetch(
+      "/content-manager/collection-types/plugins::users-permissions.user?page=1&pageSize=10&_sort=username:ASC",
+      {
+        credentials: "include",
+        mode: "cors",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE5MTU4MTYyLCJleHAiOjE2MjE3NTAxNjJ9.U1Qt-OM6VZF0L4U08aoEUNr9SnZ6M2nSZvKmy6aC96o",
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then(
+        (res) => res.results.filter((user) => user.email === adminEmail)[0].id
+      );
+    setUserID(adminUserId);
+  };
 
   const handleGoToMe = () => {
-    push({
-      pathname: `/me`,
-    });
+    // push({pathname: `/me`})
+    push(
+      `/plugins/content-manager/collectionType/plugins::users-permissions.user/${userId}`
+    );
   };
 
   const frontendUrl = sessionStorage.getItem("url");
@@ -33,7 +64,6 @@ const Logout = ({ history: { push } }) => {
   const handleLogout = () => {
     sessionStorage.removeItem("url");
     auth.clearAppStorage();
-    // push('/auth/login');
     if (typeof window !== "undefined") {
       if (frontendUrl) {
         try {
@@ -43,17 +73,15 @@ const Logout = ({ history: { push } }) => {
           );
         } catch (error) {}
       } else {
-        // push("/auth/login");
         push("/admin");
       }
-
-      // parent.window.location = window.location
     }
   };
 
   const toggle = () => setIsOpen((prev) => !prev);
 
   const userInfo = auth.getUserInfo();
+
   const displayName =
     userInfo && userInfo.firstname && userInfo.lastname
       ? `${userInfo.firstname} ${userInfo.lastname}`
